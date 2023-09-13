8760d593c598c01116ce9fedd900fc96b011f679
mehmet-bozdemir <mehmetbozdemir2000@gmail.com>
2023-09-13 09:29:53 +0200

    small fix on routes/web.php file

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
+Route::get('/', [\App\Http\Controllers\BuddyController::class, 'index']);
 Route::get('/show', [\App\Http\Controllers\IndexController::class, 'show']);
 
 Route::resource('buddy', \App\Http\Controllers\BuddyController::class);
